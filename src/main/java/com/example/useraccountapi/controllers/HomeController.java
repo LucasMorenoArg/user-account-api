a214f9619fa6f412be1aa2883a08d0d1d0af602d
Mariano Eduardo Moreno Ragni <mamoreno@ar0c02vv0p1hv2j.fibertel.com.ar>
2023-04-10 21:35:48 -0300

    RestTemplate implementación

--- src/main/java/com/example/useraccountapi/controllers/HomeController.java
+++ src/main/java/com/example/useraccountapi/controllers/HomeController.java
@@ -3,10 +3,7 @@ package com.example.useraccountapi.controllers;
 import com.example.useraccountapi.entities.Usuario;
 import com.example.useraccountapi.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
@@ -17,32 +14,27 @@ public class HomeController {
     @Autowired
     UsuarioService usuarioService;
 
-
-    RestTemplate restTemplate;
-
     @Autowired
-    public HomeController (RestTemplate restTemplate){
-        this.restTemplate=restTemplate;
+    RestTemplate usuarioRestTemplate;
 
+
+
+    @GetMapping("/prueba/{id}")
+    public Object getApi(@PathVariable Integer id){
+
+        String url="http://localhost:9000/buscarUsuario/"+id;
+        return usuarioRestTemplate.getForObject(url,Object.class);
     }
 
-    @PostMapping("/prueba")
-    public Object getApi(){
-        String url="https://localhost:9000/buscarUsuario";
-        return restTemplate.getForObject(url,Object.class);
+    @GetMapping("/buscarUsuario/{id}")
+    public Optional<Usuario> buscarUsuario(@PathVariable Integer id) {
+        Optional<Usuario> userId = usuarioService.buscarUsuario(id);
+        return userId;
     }
 
     @PostMapping("/crearUsuario")
     public void crearUsuario(@RequestBody Usuario usuario) {
         usuarioService.crearUsuario(usuario.getNombre(), usuario.getApellido(), usuario.getEmail());
-    }
-
-    @PostMapping("/buscarUsuario")
-    public Optional<Usuario> buscarUsuario(@RequestBody Usuario usuario) {
-
-        Optional<Usuario> userId = usuarioService.buscarUsuario(usuario.getIdUsuario());
-
-        return userId;
     }
 
     @PostMapping("/modificarUsuario")
